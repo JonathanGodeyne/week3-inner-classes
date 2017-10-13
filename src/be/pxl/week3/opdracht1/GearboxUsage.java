b4ee93b422a098c1a69d0721ea781949844f8ddd
11307408 <11307408@5CG7310F39>
2017-10-13 08:55:33 +0200

    OEf1 finish

--- src/be/pxl/week3/opdracht1/GearboxUsage.java
+++ src/be/pxl/week3/opdracht1/GearboxUsage.java
@@ -2,6 +2,16 @@ package be.pxl.week3.opdracht1;
 
 public class GearboxUsage {
 	public static void main(String[] args) {
+		
+//		Gearbox gearBox = new Gearbox(3);
+//		Gear gear1 = new Gear(2, 2.3);
+//		Gear gear2 = new Gear(2, 2.3);
+//		Gear gear3 = new Gear(2, 2.3);
+//		gearBox.addGear(gear1);
+//		gearBox.addGear(gear2);
+//		gearBox.addGear(gear3);
+		
+		
 		Gearbox mcLaren = new Gearbox(6);
 		mcLaren.operateClutch(true);
 		mcLaren.changeGear(1);
